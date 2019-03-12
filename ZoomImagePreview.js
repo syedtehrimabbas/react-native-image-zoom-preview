e1936022462b3c4e0241a15e7b29ed417f753b5d
Syed Tehrim Abbas <syedtehrimabbas@gmail.com>
2019-03-12 19:07:32 +0500

    add watermark image optional
    now you can pass your custom watermark with styles

--- ZoomImagePreview.js
+++ ZoomImagePreview.js
@@ -1,5 +1,7 @@
+/*react-native-image-pan-zoom
+* Syed tehrim abbas*/
 import React, {Component} from 'react';
-import {Modal, View, TouchableWithoutFeedback, StyleSheet, Image, Dimensions} from 'react-native';
+import {Modal, View, TouchableWithoutFeedback, StyleSheet, Image, Dimensions,ImageBackground} from 'react-native';
 import PropTypes from 'prop-types';
 import ImageZoom from 'react-native-image-pan-zoom';
 
@@ -8,16 +10,25 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'rgba(0,0,0,0.7)'
     },
+    backgroundImageMargin:{
+        marginLeft:10,
+        marginRight:10
+    },
     image: {
         flex: 1,
-        marginLeft: 10,
-        marginRight: 10
-    }
+        width:Dimensions.get('window').width,
+        height:Dimensions.get('window').height,
+        alignItems:'center',
+        justifyContent:'center'
+    },watermark: {opacity:0.5,alignSelf:'center',alignItems:'center',width:300,height:300}
 });
 
 export default class ZoomImagePreview extends Component {
+    static defaultProps = {
+        showWatermark:false
+    }
     render() {
-        let {source, visible, close, imageStyle, overlayStyle} = this.props;
+        let {source, visible, close, imageStyle, overlayStyle,waterMarkSource,waterMarkStyle,showWatermark} = this.props;
 
         return (<Modal
             animationType={'fade'}
@@ -30,7 +41,11 @@ export default class ZoomImagePreview extends Component {
                            imageWidth={Dimensions.get('window').width}
                            imageHeight={Dimensions.get('window').height}>
                     <TouchableWithoutFeedback onPress={close}>
-                        <Image resizeMode={'contain'} source={source} style={[styles.image, imageStyle]}/>
+                        <ImageBackground imageStyle={styles.backgroundImageMargin}resizeMode={'contain'} source={source}  style={[styles.image, imageStyle]}>
+                            <TouchableWithoutFeedback onPress={close}>
+                                <Image resizeMode={'contain'} source={waterMarkSource} style={[styles.watermark, waterMarkSource]}/>
+                            </TouchableWithoutFeedback>
+                        </ImageBackground>
                     </TouchableWithoutFeedback>
                 </ImageZoom>
             </View>
@@ -45,12 +60,19 @@ ZoomImagePreview.propTypes = {
     source: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.object
+    ]),waterMarkSource: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.object
     ]),
     indicatorProps: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.object
     ]),
+    showWatermark: PropTypes.bool,
     imageStyle: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.object
+    ]),waterMarkStyle: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.object
     ]),
