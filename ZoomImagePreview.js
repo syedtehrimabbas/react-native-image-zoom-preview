fc18198543e3475701c68150a9b421056333fb44
Syed Tehrim Abbas <syedtehrimabbas@gmail.com>
2019-03-13 13:26:27 +0500

    fix water mark width and height issue
    fix watermark width and height issue. now it will show watermark on the whole image

--- ZoomImagePreview.js
+++ ZoomImagePreview.js
@@ -1,7 +1,7 @@
 /*react-native-image-pan-zoom
 * Syed tehrim abbas*/
 import React, {Component} from 'react';
-import {Modal, View, TouchableWithoutFeedback, StyleSheet, Image, Dimensions,ImageBackground} from 'react-native';
+import {Modal, View, TouchableWithoutFeedback, StyleSheet, Image, Dimensions, ImageBackground,Alert} from 'react-native';
 import PropTypes from 'prop-types';
 import ImageZoom from 'react-native-image-pan-zoom';
 
@@ -10,46 +10,50 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'rgba(0,0,0,0.7)'
     },
-    backgroundImageMargin:{
-        marginLeft:10,
-        marginRight:10
+    backgroundImageMargin: {
+        marginLeft: 10,
+        marginRight: 10
     },
     image: {
         flex: 1,
-        width:Dimensions.get('window').width,
-        height:Dimensions.get('window').height,
-        alignItems:'center',
-        justifyContent:'center'
-    },watermark: {opacity:0.5,alignSelf:'center',alignItems:'center',width:300,height:300}
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }, watermark: {opacity: 0.5, alignSelf: 'center', alignItems: 'center', width: 300, height: 300}
 });
 
 export default class ZoomImagePreview extends Component {
-    static defaultProps = {
-        showWatermark:false
-    }
     render() {
-        let {source, visible, close, imageStyle, overlayStyle,waterMarkSource,waterMarkStyle,showWatermark} = this.props;
+        let {source, visible, close, imageStyle, overlayStyle, waterMarkSource, waterMarkStyle, showWatermark,width,height} = this.props;
 
-        return (<Modal
-            animationType={'fade'}
-            transparent={true}
-            onRequestClose={close}
-            visible={visible}>
-            <View style={[styles.overlay, overlayStyle]}>
-                <ImageZoom cropWidth={Dimensions.get('window').width}
-                           cropHeight={Dimensions.get('window').height}
-                           imageWidth={Dimensions.get('window').width}
-                           imageHeight={Dimensions.get('window').height}>
-                    <TouchableWithoutFeedback onPress={close}>
-                        <ImageBackground imageStyle={styles.backgroundImageMargin}resizeMode={'contain'} source={source}  style={[styles.image, imageStyle]}>
-                            <TouchableWithoutFeedback onPress={close}>
-                                <Image resizeMode={'contain'} source={waterMarkSource} style={[styles.watermark, waterMarkSource]}/>
-                            </TouchableWithoutFeedback>
-                        </ImageBackground>
-                    </TouchableWithoutFeedback>
-                </ImageZoom>
-            </View>
-        </Modal>);
+        return (
+            <Modal
+                animationType={'fade'}
+                transparent={true}
+                onRequestClose={close}
+                visible={visible}>
+                <View style={[styles.overlay, overlayStyle]}>
+                    <ImageZoom cropWidth={Dimensions.get('window').width}
+                               cropHeight={Dimensions.get('window').height}
+                               imageWidth={Dimensions.get('window').width}
+                               imageHeight={Dimensions.get('window').height}>
+                        <TouchableWithoutFeedback onPress={close}>
+                            <ImageBackground imageStyle={styles.backgroundImageMargin} resizeMode={'contain'}
+                                             source={source}
+                                             style={[styles.image, imageStyle]}>
+                                <TouchableWithoutFeedback style={[waterMarkStyle]} onPress={close}>
+                                    <Image resizeMode={'contain'} source={waterMarkSource}
+                                           style={[ {
+                                               width: width,
+                                               height: height
+                                           }]}/>
+                                </TouchableWithoutFeedback>
+                            </ImageBackground>
+                        </TouchableWithoutFeedback>
+                    </ImageZoom>
+                </View>
+            </Modal>);
     }
 }
 
@@ -60,7 +64,7 @@ ZoomImagePreview.propTypes = {
     source: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.object
-    ]),waterMarkSource: PropTypes.oneOfType([
+    ]), waterMarkSource: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.object
     ]),
@@ -69,10 +73,12 @@ ZoomImagePreview.propTypes = {
         PropTypes.object
     ]),
     showWatermark: PropTypes.bool,
+    height:PropTypes.number,
+    width:PropTypes.number,
     imageStyle: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.object
-    ]),waterMarkStyle: PropTypes.oneOfType([
+    ]), waterMarkStyle: PropTypes.oneOfType([
         PropTypes.number,
         PropTypes.object
     ]),
